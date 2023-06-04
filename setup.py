af7bf83c640c81c3a43c6a30442a670c6169a83f
RaulLeite2 <131724811+RaulLeite2@users.noreply.github.com>
2023-06-03 23:53:50 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LiberTech',
+    version='1.0.0',
+    description='Descrição da biblioteca',
+    author='Raul',
+    author_email='I dont go to give my email',
+    url='https://github.com/seuusuario/biblioteca',
+    packages=find_packages(),
+    install_requires=[
+        'random',
+    ],
+)
